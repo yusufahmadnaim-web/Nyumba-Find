5d78fc039bd091a391c43d4a836689ec6a61cce0
ahmadyusuf <yusufahmadnaim@gmail.com>
2026-08-05 10:40:35 +0300

    fixing the database for render

--- server/config.py
+++ server/config.py
@@ -9,8 +9,7 @@ database_url = os.getenv("DATABASE_URL")
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = database_url
+    SQLALCHEMY_DATABASE_URI = "postgresql://nyumbafind_db_user:fSkhThSYKyEHBSPkGXbw2SkqRlO5Q7sg@dpg-d9p36pm417fc73844ah0-a.oregon-postgres.render.com/nyumbafind_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
